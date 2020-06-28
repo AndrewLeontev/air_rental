801cb0d0c7d932ed20d6071c09abb76356e2f828
Andrey Leontev <leontev.andrey.s@gmail.com>
2020-06-28 15:58:29 +1000

    new views styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'static#index'
+
   devise_for :users,
              path: '',
              path_names: { sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration' },
-             controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+             controllers: { omniauth_callbacks: 'omniauth_callbacks', registrations: 'registrations' }
 
-  root 'static#index'
+  resources :users, only: [:show]
 end
